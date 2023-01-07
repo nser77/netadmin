cfa45dc258502b56f3b1faa9a8c5a6e16f8c2ca6
nser77 <104022475+nser77@users.noreply.github.com>
2023-01-07 20:21:56 +0100

    Update tasks.py

--- netadmin/worker/tasks.py
+++ netadmin/worker/tasks.py
@@ -3,9 +3,3 @@ from celery import shared_task
 @shared_task(name="worker.tasks.broad_message")
 def broad_message(message, *args, **kwargs):
     return True
-
-"""
-@shared_task
-def broad_message(message,*args, **kwargs):
-    return True
-"""
